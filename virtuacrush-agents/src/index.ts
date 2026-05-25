b24d820820c5b094ef04ce375e89941604d3f65d
Andrew <56172745+JediWebDev@users.noreply.github.com>
2026-05-24 17:53:35 -0700

    Update index.ts

--- virtuacrush-agents/src/index.ts
+++ virtuacrush-agents/src/index.ts
@@ -1,6 +1,4 @@
 import { logger, type IAgentRuntime, type Project, type ProjectAgent } from '@elizaos/core';
-// @ts-expect-error - ElizaOS SQL plugin types are not properly exported in v1.7.2
-import { sqlPlugin } from '@elizaos/plugin-sql';
 import { character as minaCharacter } from './mina.ts';
 import { character as lexiCharacter } from './lexi.ts';
 import { character as madisonCharacter } from './madison.ts';
@@ -14,25 +12,25 @@ const initCharacter = ({ runtime, name }: { runtime: IAgentRuntime, name: string
 export const minaAgent: ProjectAgent = {
   character: minaCharacter,
   init: async (runtime: IAgentRuntime) => await initCharacter({ runtime, name: minaCharacter.name }),
-  plugins: [affinityPlugin, sqlPlugin], // Added sqlPlugin
+  plugins: [affinityPlugin], 
 };
 
 export const lexiAgent: ProjectAgent = {
   character: lexiCharacter,
   init: async (runtime: IAgentRuntime) => await initCharacter({ runtime, name: lexiCharacter.name }),
-  plugins: [affinityPlugin, sqlPlugin], // Added sqlPlugin
+  plugins: [affinityPlugin],
 };
 
 export const madisonAgent: ProjectAgent = {
   character: madisonCharacter,
   init: async (runtime: IAgentRuntime) => await initCharacter({ runtime, name: madisonCharacter.name }),
-  plugins: [affinityPlugin, sqlPlugin], // Added sqlPlugin
+  plugins: [affinityPlugin],
 };
 
 export const zanderAgent: ProjectAgent = {
   character: zanderCharacter,
   init: async (runtime: IAgentRuntime) => await initCharacter({ runtime, name: zanderCharacter.name }),
-  plugins: [affinityPlugin, sqlPlugin], // Added sqlPlugin
+  plugins: [affinityPlugin],
 };
 
 const project: Project = {
